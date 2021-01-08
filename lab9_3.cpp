3d661828f3be1796a6202071dc462baf1cef0bcc
gigiftttgi <natcha_si@cmu.ac.th>
2021-01-08 16:45:41 +0700

    lab9_3_final

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,3 +1,29 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <cstdlib>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    ifstream source("score.txt");
+    int num=0;
+    double sum=0, sumsta=0, fin, avg;
+    string textline;
+    while (getline(source,textline))
+    {
+      sum += stod(textline);
+      sumsta = sumsta + stod(textline)*stod(textline);
+      num++;
+    }
+    cout << "Number of data = "<< num << '\n';
+    cout << setprecision(3);
+    avg = sum/num;
+    cout << "Mean = "<< avg << '\n';
+    fin = sumsta/num - avg*avg;
+    cout << "Standard deviation = " << sqrt(fin) <<'\n';
+    source.close();
+    return 0;
+}
